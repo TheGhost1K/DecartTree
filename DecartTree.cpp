643e00b952bdead8e5cf4bf883922981aebfcf9d
TheGhost1K <50666178+TheGhost1K@users.noreply.github.com>
2020-06-20 18:33:54 +0500

    Update DecartTree.cpp

--- DecartTree.cpp
+++ DecartTree.cpp
@@ -2,91 +2,91 @@
 #include <cassert>
 #include <algorithm>
 using namespace std;
-
+ 
 inline int gcd(int a, int b){
-	return b ? gcd(b, a%b) : a;
+        return b ? gcd(b,a%b) : a;
 }
-
+ 
 struct item {
-	int key, prior, gcd;
-	item * l, * r;
-	item() { }
-	item (int key, int prior) : key(key), prior(prior), l(NULL), r(NULL), gcd(key) { }
+        int key, prior, gcd;
+        item * l, * r;
+        item() { }
+        item (int key, int prior) : key(key), prior(prior), l(NULL), r(NULL), gcd(key) { }
 };
 typedef item * pitem;
-
+ 
 int gcd (pitem it) {
-	return it ? it->gcd : 0;
+        return it ? it->gcd : 0;
 }
-
+ 
 void upd_gcd (pitem it) {
-	if (it)
-		it->gcd = gcd(gcd(gcd(it->l),gcd(it->r)),it->key);
+        if (it)
+                it->gcd = gcd(gcd(gcd(it->l),gcd(it->r)),it->key);
 }
-
+ 
 void split (pitem t, int key, pitem & l, pitem & r) {
-	if (!t)
-		l = r = NULL;
-	else if (key < t->key)
-		split (t->l, key, l, t->l),  r = t;
-	else
-		split (t->r, key, t->r, r),  l = t;
-	upd_gcd(t);
+        if (!t)
+                l = r = NULL;
+        else if (key < t->key)
+                split (t->l, key, l, t->l),  r = t;
+        else
+                split (t->r, key, t->r, r),  l = t;
+        upd_gcd(t);
 }
-
+ 
 void insert (pitem & t, pitem it) {
-	if (!t)
-		t = it;
-	else if (it->prior > t->prior)
-		split (t, it->key, it->l, it->r),  t = it;
-	else
-		insert (it->key < t->key ? t->l : t->r, it);
-	upd_gcd(t);
+        if (!t)
+                t = it;
+        else if (it->prior > t->prior)
+                split (t, it->key, it->l, it->r),  t = it;
+        else
+                insert (it->key < t->key ? t->l : t->r, it);
+        upd_gcd(t);
 }
-
+ 
 void merge (pitem & t, pitem l, pitem r) {
-	if (!l || !r)
-		t = l ? l : r;
-	else if (l->prior > r->prior)
-		merge (l->r, l->r, r),  t = l;
-	else
-		merge (r->l, l, r->l),  t = r;
-	upd_gcd(t);
+        if (!l || !r)
+                t = l ? l : r;
+        else if (l->prior > r->prior)
+                merge (l->r, l->r, r),  t = l;
+        else
+                merge (r->l, l, r->l),  t = r;
+        upd_gcd(t);
 }
-
+ 
 void erase (pitem & t, int key) {
-	if (t->key == key)
-		merge (t, t->l, t->r);
-	else
-		erase (key < t->key ? t->l : t->r, key);
-	upd_gcd(t);
+        if (t->key == key)
+                merge (t, t->l, t->r);
+        else
+                erase (key < t->key ? t->l : t->r, key);
+        upd_gcd(t);
 }
-
+ 
 int priorities[100005];
-
+ 
 int main(){
-	freopen("input.txt","r",stdin);
-	freopen("output.txt","w",stdout);
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
 
-	int t;
-	scanf("%d\n",&t);
-	pitem root = NULL;
-	for (int i=0; i<t; ++i) priorities[i] = i;
-	random_shuffle(priorities, priorities+t);
-
-	while (t--){
-		char cmd;
-		int x;
-		scanf("%c %d\n",&cmd,&x);
-		pitem it;
-		if (cmd == '+'){
-			it = new item(x,priorities[t]);
-			insert(root, it);
-		} else if (cmd == '-'){
-			erase(root, x);
-		} else assert(false);
-
-		printf("%d\n", root == NULL ? 1 : root->gcd);
-	}
-	return 0;
+        int t;
+        scanf("%d\n",&t);
+        pitem root = NULL;
+        for (int i=0; i<t; ++i) priorities[i] = i;
+        random_shuffle(priorities, priorities+t);
+ 
+        while (t--){
+                char cmd;
+                int x;
+                scanf("%c %d\n",&cmd,&x);
+                pitem it;
+                if (cmd == '+'){
+                        it = new item(x,priorities[t]);
+                        insert(root, it);
+                } else if (cmd == '-'){
+                        erase(root, x);
+                } else assert(false);
+ 
+                printf("%d\n", root == NULL ? 1 : root->gcd);
+        }
+        return 0;
 }
